bb5882b140559dc7bd7d7ba520cf140a79d6a91c
DanielPitfield <danielpitfield1@gmail.com>
2023-08-25 17:10:22 +0100

    Twice members (data)

---
+++ app/Data/twice.ts
@@ -0,0 +1,23 @@
+import { RankableItemTemplate } from "../RankableItem";
+
+import Jihyo from "../../public/Images/Twice/jihyo.png";
+import Momo from "../../public/Images/Twice/momo.png";
+import Sana from "../../public/Images/Twice/sana.png";
+import Nayeon from "../../public/Images/Twice/nayeon.png";
+import Tzuyu from "../../public/Images/Twice/tzuyu.png";
+import Mina from "../../public/Images/Twice/mina.png";
+import Chaeyoung from "../../public/Images/Twice/chaeyoung.png";
+import Jeongyeon from "../../public/Images/Twice/jeongyeon.png";
+import Dahyun from "../../public/Images/Twice/dahyun.png";
+
+export const Twice: RankableItemTemplate[] = [
+  { id: 1, image: Jihyo, alt: "Jihyo" },
+  { id: 2, image: Momo, alt: "Momo" },
+  { id: 3, image: Sana, alt: "Sana" },
+  { id: 4, image: Nayeon, alt: "Nayeon" },
+  { id: 5, image: Tzuyu, alt: "Tzuyu" },
+  { id: 6, image: Mina, alt: "Mina" },
+  { id: 7, image: Chaeyoung, alt: "Chaeyoung" },
+  { id: 8, image: Jeongyeon, alt: "Jeongyeon" },
+  { id: 9, image: Dahyun, alt: "Dahyun" },
+];
