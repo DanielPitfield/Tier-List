248a21017b1a8b555bdc11076a2ebdf9624d39f6
DanielPitfield <danielpitfield1@gmail.com>
2023-08-27 13:10:07 +0100

    Blackpink members (data)

---
+++ app/Data/BlackpinkMembers.ts
@@ -0,0 +1,19 @@
+import { RankableItemTemplate } from "../RankableItem";
+import { StaticImageData } from "next/image";
+
+import Jennie from "../../public/Images/Blackpink Members/jennie.png";
+import Jisoo from "../../public/Images/Blackpink Members/jisoo.png";
+import Lisa from "../../public/Images/Blackpink Members/lisa.png";
+import Rose from "../../public/Images/Blackpink Members/rose.png";
+
+const BlackpinkMembersImageObjects: { image: StaticImageData; alt: string }[] = [
+  { image: Jennie, alt: "Jennie" },
+  { image: Jisoo, alt: "Jisoo" },
+  { image: Lisa, alt: "Lisa" },
+  { image: Rose, alt: "Rose" },
+];
+
+export const Blackpink: RankableItemTemplate[] = BlackpinkMembersImageObjects.map((ImageObject, Index) => ({
+  id: Index + 1,
+  ...ImageObject,
+}));
--- app/Data/TierListContextMappings.ts
+++ app/Data/TierListContextMappings.ts
@@ -1,8 +1,9 @@
 import { RankableItemTemplate } from "../RankableItem";
 import { Twice } from "./TwiceMembers";
+import { Blackpink } from "./BlackpinkMembers";
 import { F1Drivers } from "./F1Drivers";
 
-export const tierListContexts = ["Twice Members", "F1 Drivers"] as const;
+export const tierListContexts = ["Twice Members", "Blackpink Members", "F1 Drivers"] as const;
 export type TierListContext = (typeof tierListContexts)[number];
 
 type TierListContextMapping = {
@@ -12,5 +13,6 @@ type TierListContextMapping = {
 
 export const tierListContextMappings: TierListContextMapping[] = [
   { tierListContext: "Twice Members", items: Twice },
+  { tierListContext: "Blackpink Members", items: Blackpink },
   { tierListContext: "F1 Drivers", items: F1Drivers },
 ];
